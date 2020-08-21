c3f513385a84267b04aa1394972b1e4405afa03a
mark-summerfield <mark@qtrac.eu>
2020-08-21 23:58:49 +0100

    now use cppcheck

--- st.sh
+++ st.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 tokei -f -c80 -tC++,'C++ Header'
+cppcheck -q --enable=all . 2>&1 \
+    | grep -v application.*OnInit..is.never.used \
+    | grep -v artprovider.*CreateBitmap..is.never.used \
+    | grep -v boardutil.*operator==..is.never.used \
+    | grep -v boardwidget.*DoGetBestClientSize..is.never.used \
+    | grep -v Cppcheck.cannot.find.all.the.include.files
 git st
